6aa96c36eaf16725fe8738bddee9213c19f56b8c
yydrowz3 <yydrowz3@gmail.com>
2026-08-16 18:07:06 +0800

    🚧 chore: frontend dependencies initialize with vite

---
+++ web/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+});
